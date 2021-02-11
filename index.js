8fdcc69c743440521a933594cd1a97ed89ea40a1
Plofland <ploflan@gmail.com>
2021-02-11 14:29:44 -0500

    Fixed backend path bug

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const dotenv = require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const path = require("path")
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 9000;
 
-const app = express()
-const port = process.env.PORT || 9000
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.static(path.join(__dirname, 'client/build'))
+); //this is telling express to set the static page as the home page, join combines all the parts to make a URL
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "client/build", "index.html")))//this is telling express to set the static page as the home page, join combines all the parts to make a URL
+app.use('/api/*', (_, res) => {
+  res.json({ data: 'The API lives!!!' });
+});
 
-
-app.use('/api/*', ( _, res) => {
-  res.json({data: 'The API lives!!!'})
-})
+app.use('/*', (_, res) => {
+  res.sendFile(
+    path.join(__dirname, 'client/build', 'index.html')
+  );
+});
 
 app.listen(port, () => {
-  console.log(`Server is alive on port ${port}`)
-})
-
-
-
-
+  console.log(`Server is alive on port ${port}`);
+});
 
 // console.log("It's alive!")
 // console.log(__dirname)
@@ -34,5 +36,3 @@ app.listen(port, () => {
 // console.log(dotenv.FOOD)//undefined, this does not work
 // console.log(dotenv)
 // console.log(dotenv.parsed)
-
-
